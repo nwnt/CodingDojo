0c67a6b03b23564cd7c3fb8a064ce08bb92196fb
Nont <nontkrub@gmail.com>
2021-04-18 18:54:29 -0500

    Add first spare case

--- src/main/java/dev/nont/kata/bowling/BowlingGame.java
+++ src/main/java/dev/nont/kata/bowling/BowlingGame.java
@@ -2,11 +2,27 @@ package dev.nont.kata.bowling;
 
 public class BowlingGame {
 
-    public BowlingGame(String result) {
+    private final String result;
 
+    public BowlingGame(String result) {
+        this.result = result;
     }
 
     public int getScore() {
-        return 60;
+        var total = 0;
+        char[] carray = result.toCharArray();
+        for (int i = 0; i < carray.length; i++) {
+            if (Character.isDigit(carray[i])) {
+                total += Character.getNumericValue(carray[i]);
+                continue;
+            }
+            switch (carray[i]) {
+                case '/':
+                    total -= carray[i-1];
+                    total += 10 + carray[i+1];
+                    break;
+            }
+        }
+        return total;
     }
 }
--- src/test/java/dev/nont/kata/bowling/BowlingGameTest.java
+++ src/test/java/dev/nont/kata/bowling/BowlingGameTest.java
@@ -7,11 +7,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BowlingGameTest {
 
-    @DisplayName("Test a valid game")
+    @DisplayName("No strikes and no spares is a sum of all points")
     @Test
     public void validGame() {
         var result = "12345123451234512345";
         var expect = 60;
+        BowlingGame sut = new BowlingGame(result);
+        assertThat(sut.getScore())
+                .isEqualTo(expect);
+    }
+
+    @DisplayName("A - means a miss (0 score)")
+    @Test
+    public void heartbreak9pins() {
+        var result = "9-9-9-9-9-9-9-9-9-9-";
+        var expect = 90;
+        BowlingGame sut = new BowlingGame(result);
+        assertThat(sut.getScore())
+                .isEqualTo(expect);
+    }
+
+    @DisplayName("A spare in mid game")
+    @Test
+    public void aSpareMidGame() {
+        var result = "9/111111111111111111";
+        var expect = 29;
         BowlingGame sut = new BowlingGame(result);
         assertThat(sut.getScore())
                 .isEqualTo(expect);
